c8664f9d675078493167e3a4f88c49698b02fa8b
ekyeon715 <rkdwldus9636@naver.com>
2024-10-02 16:32:41 +0900

    feat: 타입정의

---
+++ src/types/Champion.ts
@@ -0,0 +1,12 @@
+export interface Champion {
+  key: string;
+  name: string;
+  title: string;
+  blurb: string;
+  info: {
+    attack: number;
+    defense: number;
+    magic: number;
+    difficulty: number;
+  };
+}
---
+++ src/types/Items.ts
@@ -0,0 +1,9 @@
+interface Items {
+  name: string;
+  plaintext: string;
+  gold: {
+    base: number;
+    total: number;
+    sell: number;
+  };
+}
