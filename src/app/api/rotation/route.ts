f589b1cf494261bdcda017a7554956a36c372dfd
ekyeon715 <rkdwldus9636@naver.com>
2024-10-02 16:33:33 +0900

    feat: 로테이션 데이터 페칭

---
+++ src/app/api/rotation/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+
+export async function getRotationChamp(request: Request) {
+  const response = await fetch(
+    "https://kr.api.riotgames.com/lol/platform/v3/champion-rotations",
+    {
+      method: "GET",
+      headers: {
+        "X-Riot-Token": process.env.RIOT_API_KEY as string,
+      },
+    }
+  );
+  const data = await response.json();
+}
