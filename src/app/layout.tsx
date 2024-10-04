ddf2368af805c318dcd2ed5eee57adbc66a7fe3c
ekyeon715 <rkdwldus9636@naver.com>
2024-10-04 16:01:36 +0900

    fix: children 위치 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,13 +15,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {children}
         <nav>
           <Link href="/">홈</Link>
           <Link href="/champions">챔피언 목록</Link>
           <Link href="/items">아이템 목록</Link>
           <Link href="/rotation">챔피언 로테이션</Link>
         </nav>
+        {children}
       </body>
     </html>
   );
