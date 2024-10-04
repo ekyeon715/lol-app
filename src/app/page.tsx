e1d958524fe7a6eae8587882136a977bc1fd3735
ekyeon715 <rkdwldus9636@naver.com>
2024-10-04 15:54:27 +0900

    fix: 사진 경로 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,15 +8,15 @@ const page = () => {
       <p>리그오브레전드 정보 앱</p>
       <p>Riot Games API를 활용하여 챔피언과 아이템 정보를 제공합니다.</p>
       <Link href="/champions">
-        <Image src="champ.png" alt="" />
+        <Image src="/champ.png" alt="챔피언 목록" width={400} height={300} />
         <p>챔피언 목록 보기</p>
       </Link>
       <Link href="/lotation">
-        <Image src="lol.png" alt="" />
+        <Image src="/lol.png" alt="금주 로테이션" width={400} height={300} />
         <p>금주 로테이션 확인</p>
       </Link>
       <Link href="/items">
-        <Image src="mejai.png" alt="" />
+        <Image src="/mejai.png" alt="아이템 목록" width={400} height={300} />
         <p>아이템 목록 보기</p>
       </Link>
     </div>
