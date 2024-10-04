28403b72476034280f2936f163df643c994dcb25
ekyeon715 <rkdwldus9636@naver.com>
2024-10-04 15:53:30 +0900

    fix: 바디태그 위치 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,13 +14,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
-      <nav>
-        <Link href="/">홈</Link>
-        <Link href="/champions">챔피언 목록</Link>
-        <Link href="/items">아이템 목록</Link>
-        <Link href="/rotation">챔피언 로테이션</Link>
-      </nav>
+      <body>
+        {children}
+        <nav>
+          <Link href="/">홈</Link>
+          <Link href="/champions">챔피언 목록</Link>
+          <Link href="/items">아이템 목록</Link>
+          <Link href="/rotation">챔피언 로테이션</Link>
+        </nav>
+      </body>
     </html>
   );
 }
