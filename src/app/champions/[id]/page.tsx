39401bfee85df8301eb464c338040c5104a1cd5c
ekyeon715 <rkdwldus9636@naver.com>
2024-10-04 16:26:01 +0900

    feat: 메타데이터 추가

--- src/app/champions/[id]/page.tsx
+++ src/app/champions/[id]/page.tsx
@@ -2,6 +2,15 @@ import { getChampionDetail } from "@/utils/severApi";
 
 export const dynamic = "force-dynamic";
 
+export async function generateMetadata({ params }: { params: { id: string } }) {
+  const champion = await getChampionDetail(params.id);
+
+  return {
+    title: `${champion.name} - ${champion.title}`,
+    description: champion.lore.substring(0, 150),
+  };
+}
+
 const DetailPage = async ({ params }: { params: { id: string } }) => {
   const championDetail = await getChampionDetail(params.id);
   return <div></div>;
