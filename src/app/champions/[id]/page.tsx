670ca1bcf728abde3108f392bb561e00e367f07d
ekyeon715 <rkdwldus9636@naver.com>
2024-10-04 17:02:18 +0900

    feat: 상세페이지 내용 추가

--- src/app/champions/[id]/page.tsx
+++ src/app/champions/[id]/page.tsx
@@ -1,4 +1,6 @@
+import { Champion } from "@/types/Champion";
 import { getChampionDetail } from "@/utils/severApi";
+import Image from "next/image";
 
 export const dynamic = "force-dynamic";
 
@@ -7,13 +9,32 @@ export async function generateMetadata({ params }: { params: { id: string } }) {
 
   return {
     title: `${champion.name} - ${champion.title}`,
-    description: champion.lore.substring(0, 150),
+    description: champion.lore,
   };
 }
 
 const DetailPage = async ({ params }: { params: { id: string } }) => {
-  const championDetail = await getChampionDetail(params.id);
-  return <div></div>;
+  const champion: Champion = await getChampionDetail(params.id);
+  return (
+    <div>
+      <h1>{champion.name}</h1>
+      <h2>{champion.title}</h2>
+      <Image
+        src={`https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion.id}_0.jpg`}
+        alt={`${champion.name} 이미지`}
+        width={400}
+        height={400}
+      />
+      <p>{champion.lore}</p>
+      <h3>스탯</h3>
+      <ul>
+        <li>공격력: {champion.info.attack}</li>
+        <li>방어력: {champion.info.defense}</li>
+        <li>마법력: {champion.info.magic}</li>
+        <li>난이도: {champion.info.difficulty}</li>
+      </ul>
+    </div>
+  );
 };
 
 export default DetailPage;
