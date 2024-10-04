9f39b68c2fd8ab1963dc5251c572cb657b3d1d71
ekyeon715 <rkdwldus9636@naver.com>
2024-10-04 15:02:41 +0900

    feat: 챔피언 목록 페이지 추가

---
+++ src/app/champions/page.tsx
@@ -0,0 +1,30 @@
+import { getChampionList, getLatestVersion } from "@/utils/severApi";
+import Image from "next/image";
+
+export const revalidate = 86400;
+
+const page = async () => {
+  const [version, champions] = await Promise.all([
+    getLatestVersion(),
+    getChampionList(),
+  ]);
+  return (
+    <div>
+      <p>챔피언 목록</p>
+      {champions.map((champion) => (
+        <div key={champion.key}>
+          <Image
+            src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${champion.image.full}`}
+            alt={champion.name}
+            width={100}
+            height={100}
+          />
+          <h2>{champion.name}</h2>
+          <p>{champion.title}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default page;
