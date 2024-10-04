5543f7e9930ff1f11e2944875dcfe920e2c45651
ekyeon715 <rkdwldus9636@naver.com>
2024-10-04 15:03:18 +0900

    fix: 챔피언 타입 정의 추가

--- src/types/Champion.ts
+++ src/types/Champion.ts
@@ -1,8 +1,13 @@
 export interface Champion {
+  id: string;
   key: string;
   name: string;
   title: string;
   lore: string;
+  image: {
+    full: string;
+    sprite: string;
+  };
   info: {
     attack: number;
     defense: number;
